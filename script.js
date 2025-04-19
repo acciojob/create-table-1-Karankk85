c38fc3930ff2b4c883e46ae8382ffca8ff62c611
Karankk85 <karkk8533@gmail.com>
2025-04-19 10:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function insert_Row() {
+
     let table = document.getElementById("sampleTable");
-  
+
+
     let newRow = table.insertRow(0);
 
-	let cell1 = newRow.insertcell(0);
-	let cell2 = newRow.insertcell(1);
+ 
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
 
-	cell1.innerHTML = "New cell1";
-	cell2.innerHTML = "New cell2"
+
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
 }
